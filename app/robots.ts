2b49bdd13b4366e3750687d2760911acc8495642
kythemogul <kmontrell@xloudone.com>
2025-05-19 02:23:53 -0400

    Add static sitemap.xml and update robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -8,5 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ['/private/', '/admin/'],
     },
     sitemap: 'https://xognito.com/sitemap.xml',
+    host: 'https://xognito.com',
   }
 } 
